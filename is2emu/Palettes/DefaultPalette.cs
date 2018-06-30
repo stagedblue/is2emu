5cc985ad5b6cb0a24b9bcdd24db732e4d6d48075
stagedblue <40707175+stagedblue@users.noreply.github.com>
2018-06-29 23:32:47 -0400

    Add palette support
    This commit will add:
    - Palette.cs: Base palette class that palettes will inherit
    - DefaultPalette.cs: Default palette (official colors from TWC)

---
+++ is2emu/Palettes/DefaultPalette.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+namespace is2emu.Palettes
+{
+    class DefaultPalette : Palette
+    {
+        private DefaultPalette() {
+            rainPalette = new Palette.dBZBase
+            {
+                dBZ5 = new Color(128, 225, 128),
+                dBZ10 = new Color(64, 160, 64),
+                dBZ20 = new Color(32, 112, 32),
+                dBZ30 = new Color(0, 65, 0),
+                dBZ35 = new Color(255, 255, 0),
+                dBZ45 = new Color(225, 127, 0),
+                dBZ55 = new Color(255, 0, 0),
+                dBZ65 = new Color(128, 0, 0),
+                dBZ75 = new Color(254, 85, 254),
+                dBZ85P = new Color(255, 255, 255)
+            };
+            mixPalette = new Palette.dBZBase
+            {
+                dBZ5 = new Color(255, 192, 192),
+                dBZ30 = new Color(255, 158, 158),
+                dBZ45 = new Color(255, 124, 124),
+                dBZ55 = new Color(255, 99, 99)
+            };
+            snowPalette = new Palette.dBZBase
+            {
+                dBZ5 = new Color(55, 255, 255),
+                dBZ10 = new Color(43, 212, 254),
+                dBZ20 = new Color(31, 169, 255),
+                dBZ30 = new Color(19, 126, 255),
+                dBZ35 = new Color(11, 97, 255),
+                dBZ45 = new Color(0, 55, 255)
+            };
+            // The above palettes are "official" (straight from TWC API docs.)
+        }
+
+        private static readonly DefaultPalette instance = new DefaultPalette();
+        public static DefaultPalette GetInstance()
+        {
+            return instance;
+        }
+    }
+}
---
+++ is2emu/Palettes/Palette.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+namespace is2emu
+{
+    public class Palette
+    {
+        /// <summary>
+        /// Base for palettes based upon dBZ scale below (most is optional)
+        /// </summary>
+        public class dBZBase
+        {
+            /// <summary>
+            /// Color used for less than or equal to 5 dBZ
+            /// </summary>
+            public Color dBZ5;
+            public Color dBZ10;
+            public Color dBZ20;
+            public Color dBZ30;
+            public Color dBZ35;
+            public Color dBZ45;
+            public Color dBZ55;
+            public Color dBZ65;
+            public Color dBZ75;
+            /// <summary>
+            /// Color used for 85 dBZ or higher
+            /// </summary>
+            public Color dBZ85P;
+        }
+
+        public dBZBase rainPalette;
+        public dBZBase mixPalette;
+        public dBZBase snowPalette;
+    }
+}
